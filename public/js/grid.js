a7aa2c4175a0930005c7970f782b64c06a8eb53e
unknown <Amiram.ab@gmail.com>
2022-10-21 18:55:49 +0300

    Start with creating world and js funcs

---
+++ public/js/grid.js
@@ -0,0 +1,9 @@
+/**
+ * @description Create new world
+ * @param {number}
+ */
+export const drawGrid = (arr2D) => {
+    for (let i = 0; i < arr2D.length; i++) {
+        for (let j = 0; j < arr2D[i].length; j++) {}
+    }
+};

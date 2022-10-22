20a8a389b826219eb846f45b7bab0ad32cf675a2
unknown <Amiram.ab@gmail.com>
2022-10-22 10:05:17 +0300

    New world generate function added

--- app/app.js
+++ app/app.js
@@ -1,3 +1,10 @@
 import { start as handleStart } from "../public/js/landPage.js";
+import { drawGrid as draw } from "../public/js/grid.js";
+import { northWorld } from "../public/js/worlds.js";
+//landPage
 const startBtn = document.querySelector("#start");
 startBtn.addEventListener("click", handleStart);
+//main
+const gameBoard = document.getElementById("game-board");
+function main() {}
+draw(northWorld, gameBoard);
--- public/js/grid.js
+++ public/js/grid.js
@@ -2,8 +2,33 @@
  * @description Create new world
  * @param {number}
  */
-export const drawGrid = (arr2D) => {
+export const drawGrid = (arr2D, board) => {
     for (let i = 0; i < arr2D.length; i++) {
-        for (let j = 0; j < arr2D[i].length; j++) {}
+        for (let j = 0; j < arr2D[i].length; j++) {
+            const gameElement = document.createElement("div");
+            switch (arr2D[i][j]) {
+                case 2:
+                    gameElement.classList.add("cloud-tile");
+                    break;
+                case 3:
+                    gameElement.classList.add("grass-tile");
+                    break;
+                case 4:
+                    gameElement.classList.add("soil-tile");
+                    break;
+                case 5:
+                    gameElement.classList.add("tree-tile");
+                    break;
+                case 6:
+                    gameElement.classList.add("tree-leaves-tile");
+                    break;
+                case 7:
+                    gameElement.classList.add("rock-tile");
+                    break;
+                default:
+                    break;
+            }
+            board.appendChild(gameElement);
+        }
     }
 };
